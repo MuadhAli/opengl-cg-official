a86e5e5d5ff7a6acd8a314fd3d75247eb088c747
muadhali <mkhanmanzil@gmail.com>
2023-07-08 18:12:50 +0530

    updated the file

--- README.md
+++
@@ -1,3 +0,0 @@
-# 3D-Classroom-OpenGL
-
-Video: https://www.youtube.com/watch?v=SV-CJTGWdUA
--- intro.cpp
+++ intro.cpp
@@ -2,35 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Function to be executed when a specific key is pressed
 void executeFunction(unsigned char key) {
     switch (key) {
-        case 13: // ASCII code for Enter key
-            system("./classroom"); // Command for Enter key
+        case 13: 
+            system("./classroom");
             break;
         case 'a':
-            system("./dino"); // Command for key 'a'
+            system("./dino"); 
             break;
         case 'b':
-            system("./tower"); // Command for key 'b'
+            system("./scenery");
             break;
         case 'c':
-            system("./digger"); // Command for key 'c'
+            system("./ddd"); 
             break;
         case 'd':
-            system("./demo"); // Command for key 'd'
+            system("./demo"); 
             break;
         default:
             break;
     }
 }
 
-// Function to handle keyboard events
 void handleKeypress(unsigned char key, int x, int y) {
     executeFunction(key);
 }
 
-// Function to handle drawing on the screen
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -42,9 +39,7 @@ void display() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // Draw your front page design here
 
-    // Set color to red and draw the first square
     glColor3f(1.0f, 0.0f, 0.0f);
     glBegin(GL_QUADS);
     glVertex2f(50, 100);
@@ -53,7 +48,6 @@ void display() {
     glVertex2f(50, 200);
     glEnd();
 
-    // Set color to green and draw the second square
     glColor3f(0.0f, 1.0f, 0.0f);
     glBegin(GL_QUADS);
     glVertex2f(250, 100);
@@ -62,8 +56,9 @@ void display() {
     glVertex2f(250, 200);
     glEnd();
 
-    // Set color to blue and draw the third square
-    glColor3f(0.0f, 0.0f, 1.0f);
+
+
+    glColor3f(1.0f, 1.0f, 0.0f);
     glBegin(GL_QUADS);
     glVertex2f(450, 100);
     glVertex2f(550, 100);
@@ -71,35 +66,24 @@ void display() {
     glVertex2f(450, 200);
     glEnd();
 
-    // Set color to yellow and draw the fourth square
-    glColor3f(1.0f, 1.0f, 0.0f);
-    glBegin(GL_QUADS);
-    glVertex2f(650, 100);
-    glVertex2f(750, 100);
-    glVertex2f(750, 200);
-    glVertex2f(650, 200);
-    glEnd();
-
-    // Set color to white for text
     glColor3f(1.0f, 1.0f, 1.0f);
-    glRasterPos2f(280, 300); // Position the text in the middle
+    glRasterPos2f(280, 300); 
     const char* text1 = "To see the classroom, press Enter";
     for (const char* c = text1; *c != '\0'; ++c) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); 
     }
         glColor3f(1.0f, 1.0f, 1.0f);
-    glRasterPos2f(280, 270); // Position the text in the middle
+    glRasterPos2f(280, 270); 
     const char* text10 = "To see the Computer Lab, press D";
     for (const char* c = text10; *c != '\0'; ++c) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); 
     }
 
-    // Set color to white for top text
     glColor3f(1.0f, 1.0f, 1.0f);
     glRasterPos2f(280, 550); // Position the top text
     const char* text2 = "welcome to the 3D simulation";
     for (const char* c = text2; *c != '\0'; ++c) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c);
     }
 
         glColor3f(1.0f, 1.0f, 1.0f);
@@ -133,16 +117,11 @@ void display() {
     }
                 glColor3f(1.0f, 1.0f, 1.0f);
     glRasterPos2f(580, 340); // Position the top text
-    const char* text8 = "Press B to play Diggerman Game";
+    const char* text8 = "Press B to see the scenery";
     for (const char* c = text8; *c != '\0'; ++c) {
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
     }
-                glColor3f(1.0f, 1.0f, 1.0f);
-    glRasterPos2f(580, 310); // Position the top text
-    const char* text9 = "Press C to play Towe of Hanoi Game";
-    for (const char* c = text9; *c != '\0'; ++c) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
-    }
+
                 glColor3f(0.0f, 0.0f, 0.0f);
     glRasterPos2f(80, 150); // Position the top text
     const char* text11 = "Press A";
@@ -157,7 +136,7 @@ void display() {
     }
                 glColor3f(0.0f, 0.0f, 0.0f);
     glRasterPos2f(480, 150); // Position the top text
-    const char* text13 = "Press C";
+    const char* text13 = "Press D";
     for (const char* c = text13; *c != '\0'; ++c) {
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
     }
@@ -167,10 +146,6 @@ void display() {
     for (const char* c = text15; *c != '\0'; ++c) {
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c); // Use a bitmap font for the text
     }
-
-
-
-
     glutSwapBuffers();
 }
 
@@ -180,7 +155,6 @@ int main(int argc, char** argv) {
     glutInitWindowSize(800, 600);
     glutCreateWindow("Front Page");
 
-    // Register display and keyboard callback functions
     glutDisplayFunc(display);
     glutKeyboardFunc(handleKeypress);
 
